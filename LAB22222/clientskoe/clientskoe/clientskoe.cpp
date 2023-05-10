1944028feb71751c288d2b4c0a5571105b4078f0
Samsonov-aleksey <71924957+Samsonov-aleksey@users.noreply.github.com>
2023-05-10 15:46:20 +0300

    LAB22222

---
+++ LAB22222/clientskoe/clientskoe/clientskoe.cpp
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>  
+#include<string.h>
+
+int main() {
+	FILE* file;
+	if (fopen_s(&file, "C:/Users/Алексей/Desktop/test_file.alexeytxt", "r+")) {
+		printf_s("Error on open file!\n");
+		getchar();
+		return 0;
+	}
+
+	char buffer[1024] = { 0 };
+	fread(buffer, sizeof(char), 1024, file);
+	printf_s("% s", buffer);
+	printf_s("\n");
+
+
+
+	memset(buffer, '\0', 1024);
+	strcat_s(buffer, "Samsonov_Aleks");
+
+	fseek(file, 0, 0);
+	fwrite(buffer, sizeof(char), 1024, file);
+
+	fclose(file);
+
+	getchar();
+
+	return 0;
+
+
+}
